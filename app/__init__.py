89dd04907a2d08dced5f703a2e195be406c903fe
Jenna <jennagodfrey324@gmail.com>
2023-06-13 16:22:32 -0700

    finished second feature, read, create and delete suspects

--- app/__init__.py
+++ app/__init__.py
@@ -9,6 +9,7 @@ from .api.user_routes import user_routes
 from .api.auth_routes import auth_routes
 from .api.notes_routes import notes_routes
 from .api.suspect_routes import suspect_routes
+from .api.character_routes import character_routes
 from .seeds import seed_commands
 from .config import Config
 
@@ -32,6 +33,7 @@ app.register_blueprint(user_routes, url_prefix='/api/users')
 app.register_blueprint(auth_routes, url_prefix='/api/auth')
 app.register_blueprint(notes_routes, url_prefix='/api/notes')
 app.register_blueprint(suspect_routes, url_prefix='/api/suspects')
+app.register_blueprint(character_routes, url_prefix='/api/characters')
 
 db.init_app(app)
 Migrate(app, db)
--- react-app/src/components/OpeningScene/SuspectModal.js
+++ react-app/src/components/OpeningScene/SuspectModal.js
@@ -1,30 +1,70 @@
 import { useDispatch, useSelector } from 'react-redux'
 import { useEffect } from 'react';
-import { getSuspects } from '../../store/suspects';
-
+import { deleteSuspect, getSuspects, postSuspect } from '../../store/suspects';
+import { getCharacters } from '../../store/characters';
 import './OpeningScene.css'
 
 
 
 const SuspectModal = () => {
     const dispatch = useDispatch();
+    const characters = useSelector(state => state.characters)
     const suspects = Object.values(useSelector(state => state.suspects))
 
 
     useEffect(() => {
+        dispatch(getCharacters())
         dispatch(getSuspects())
     }, [dispatch])
 
 
+    const handleDelete = async (e, suspect) => {
+        e.preventDefault();
+
+        await dispatch(deleteSuspect(suspect.id))
+    }
+
+    const handleAddBack = async (e, id) => {
+        e.preventDefault();
+
+        const formData = new FormData()
+        formData.append('id', id)
+
+        await dispatch(postSuspect(formData))
+    }
+
+
     if (!suspects) return null;
+
+    const characterIds = []
+    for (const character of Object.values(characters)) {
+        characterIds.push(character.id)
+    }
+    const suspectIds = []
+    for (const suspect of suspects) {
+        suspectIds.push(suspect.character_id)
+    }
+
+    const addBackIds = []
+    for (const id of characterIds) {
+        if (!suspectIds.includes(id)) {
+            addBackIds.push(id)
+        }
+    }
 
     return (
         <div className='suspect-container'>
             {suspects.map(suspect => {
                 return (
-                    <div className='single-suspect'>
+                    <div className='single-suspect' key={suspect.id}>
                         <p>{suspect.first_name} {suspect.last_name}</p>
+                        <button onClick={(e) => handleDelete(e, suspect)}>remove suspect</button>
                     </div>
+                )
+            })}
+            {addBackIds.map(id => {
+                return (
+                    <button key={id} onClick={(e) => handleAddBack(e, id)}>add back {characters[id]['first_name']} {characters[id]['last_name']}</button>
                 )
             })}
         </div>
