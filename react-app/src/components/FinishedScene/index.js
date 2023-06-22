3b419a82b64d64e2250026791252dad2255c7c15
Jenna <jennagodfrey324@gmail.com>
2023-06-21 18:23:21 -0700

    did congrats styling

--- react-app/src/components/FinishedScene/index.js
+++ react-app/src/components/FinishedScene/index.js
@@ -1,14 +1,17 @@
 import { useHistory } from "react-router-dom"
-
+import './finishScene.css'
 
 const FinishedScene = () => {
     const history = useHistory();
 
     return (
-        <div className="congrats-house">
-            <h1>Congratulations!</h1>
-            <p>You have upkept your reputation as detective and solved the crime in no time. Thank you for all your help. Click the button bellow to exit the game...</p>
-            <button onClick={() => history.push('/signup')}>Return</button>
+        <div className="total-congrats-house">
+            <img src="https://i.imgur.com/1HDvBws.jpg" alt="office background"></img>
+            <div className="congrats-house">
+                <h1>Congratulations!</h1>
+                <p>You have upkept your reputation as detective and solved the crime in no time. Thank you for all your help. Click the button bellow to exit the game...</p>
+                <button onClick={() => history.push('/signup')}>Return</button>
+            </div>
         </div>
     )
 }
