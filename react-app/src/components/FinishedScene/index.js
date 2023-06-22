5a4dc8033ae088ce2b87d1d24621a4c5caccd8e1
Jenna <jennagodfrey324@gmail.com>
2023-06-21 17:37:08 -0700

    got the submit button fixed

--- react-app/src/components/FinishedScene/index.js
+++ react-app/src/components/FinishedScene/index.js
@@ -1,27 +1,15 @@
-import { useDispatch, useSelector } from "react-redux";
-import { logout } from "../../store/session";
-import { Redirect } from "react-router-dom/cjs/react-router-dom.min";
+import { useHistory } from "react-router-dom"
 
 
 const FinishedScene = () => {
-    const dispatch = useDispatch();
-    const user = useSelector(state => state.session.user)
-
-
-    const handleLogout = async (e) => {
-		e.preventDefault();
-		await fetch('/api/reset/');
-		dispatch(logout());
-	};
-
-    if (!user) return <Redirect to='/signup'/>
+    const history = useHistory();
 
     return (
-        <>
+        <div className="congrats-house">
             <h1>Congratulations!</h1>
             <p>You have upkept your reputation as detective and solved the crime in no time. Thank you for all your help. Click the button bellow to exit the game...</p>
-            <button onClick={handleLogout}>End Game</button>
-        </>
+            <button onClick={() => history.push('/signup')}>Return</button>
+        </div>
     )
 }
 
