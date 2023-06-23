6501c0cdb2e914757dd7b937350fce4e83358dd8
Jenna <jennagodfrey324@gmail.com>
2023-06-22 21:09:15 -0700

    render debugging

--- app/api/reset.py
+++ app/api/reset.py
@@ -10,15 +10,18 @@ reset = Blueprint('reset', __name__)
 @login_required
 def reset_seeds():
 
-    old_demo_user = current_user.username
+    if current_user.id == 1:
+        demo_user = User.query.get(current_user.id)
+        [db.session.delete(note) for note in demo_user.notes]
+        [db.session.delete(suspect) for suspect in demo_user.suspects]
+        [db.session.delete(backpack) for backpack in demo_user.backpack_items]
+        [db.session.delete(place) for place in demo_user.places]
+
+        db.session.commit()
+        return {'message': 'Successfully reset'}
+
     user = User.query.get(current_user.id)
     db.session.delete(user)
     db.session.commit()
-
-    if old_demo_user == 'Demo':
-        demo = User(
-        username='Demo', first_name='Demo', last_name='Lition', password='password')
-        db.session.add(demo)
-        db.session.commit()
 
     return {'message': 'Successfully reset'}
